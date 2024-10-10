695e6ae6555f32008cdb5750a705f730f22d692f
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-10-10 16:48:49 +0530

    Update root route to serve HTML documentation for API endpoints

--- api/server.js
+++ api/server.js
@@ -17,7 +17,24 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: t
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the social media API');
+  //send html for frontend
+  res.send(`<html>
+            <h1>Pip Trade Social media api</h1>
+            <h3>Endpoints</h3>
+            <h4>Users</h4>
+            <p>Feed:-   GET /api/users/feed</p>
+            <p>New User Register:-  POST /api/users/register</p>
+            <p>User Login:-  POST /api/users/login</p>
+            <h4>Posts</h4>
+            <p>Create Post:-  POST /api/posts</p>
+            <p>Add Comment:-  POST /api/posts/:postId/comment</p>
+            
+            <h4>Friends</h4>
+            <p>Send Friend Request:-  POST /api/friends/request/:userId</p>
+            <p>Accept Friend Request:-  POST /api/friends/accept/:userId</p>
+            <p>Reject Friend Request:-  POST /api/friends/reject/:userId</p>
+
+        </html>`)
 });
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
